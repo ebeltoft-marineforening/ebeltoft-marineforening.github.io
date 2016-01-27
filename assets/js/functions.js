0071b7d7e0a7888930ae8dde29b1de5abf07dca7
Hans Kristoffer <hans.kristoffer@hotmail.dk>
2016-01-27 10:01:10 +0100

    u

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,5 +1,13 @@
 $(function() {
 
+  var pathname = window.location.pathname;
+
+  $(".menu a").each(function(index) {
+    if (pathname === $(this).attr('href') ) {
+      $(this).addClass("active");
+    }
+  });
+
   $('.mobile-nav-toggle').on('click', function(){
     var status = $(this).hasClass('is-open');
     if(status){ $('.mobile-nav-toggle, #page-header').removeClass('is-open'); }
